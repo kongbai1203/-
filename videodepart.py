a31995e4d6f3fb896ff94d366e2c3d1aab70faea
kongbai1203 <m202572855@hust.edu.cn>
2025-12-29 12:50:44 +0800

    Update videodepart.py
    优化了使用者的修改设置流程

--- videodepart.py
+++ videodepart.py
@@ -3,6 +3,20 @@ import os
 import time
 import re
 
+# ============== 只需修改这里 ==============
+# 配置参数
+base_dir = "D:/study/work" #新建“test_vedio”文件夹用于存放视频，配置“test_vedio”所在路径
+output_base = "D:/study/work/test_photo/" #配置输出文件夹所在路径
+# μF_values = [2]  # 根据实际情况修改
+# suffixes = [1,2,3,4]
+
+# #批量处理
+# for uF in μF_values:
+#     for suffix in suffixes:
+        # video_name = f"25μs_{uF}μF_200FPS_200Hz_{suffix}.avi"
+video_name = f"MB7_201HZ_200FPS_12_1.mp4" #设置所处理视频名称
+video_path = os.path.join(base_dir, "test_vedio", video_name)
+# ==========================================
 def process_video(video_path, output_base):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
@@ -28,20 +42,8 @@ def process_video(video_path, output_base):
     print(f"处理完成: {video_path} | 耗时: {time.time() - start_time:.2f}s | 生成帧数: {frame_count}")
 
 
-# 配置参数
-base_dir = "D:/study/work"
-output_base = "D:/study/work/test_photo/"
-# μF_values = [2]  # 根据实际情况修改
-# suffixes = [1,2,3,4]
-
-# #批量处理
-# for uF in μF_values:
-#     for suffix in suffixes:
-        # video_name = f"25μs_{uF}μF_200FPS_200Hz_{suffix}.avi"
-video_name = f"MB7_201HZ_200FPS_12_1.mp4"
-video_path = os.path.join(base_dir, "test_vedio", video_name)
-
 if os.path.exists(video_path):
     process_video(video_path, output_base)
 else:
     print(f"文件不存在: {video_path}")
+
